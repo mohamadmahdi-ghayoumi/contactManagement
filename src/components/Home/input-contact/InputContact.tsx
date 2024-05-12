99edce22292cd0657a654ba04da84e04ffd4a5f8
mohamadghayoumi <mohamad.aftabi75@gmail.com>
2024-05-13 00:29:18 +0330

    feat:delete cmnt

--- src/components/Home/input-contact/InputContact.tsx
+++ src/components/Home/input-contact/InputContact.tsx
@@ -1,29 +1,4 @@
-// import React, { useState } from 'react'
 
-// function InputContact({title , setValue}: {title : string , setValue:React.Dispatch<React.SetStateAction<string>>}) {
-//   const [inputValue, setInputValue] = useState('');
-
-// function handleTitle(e: React.ChangeEvent<HTMLInputElement>){
-//   setValue(e.target.value)
-//   setInputValue(e.target.value)
-
-// }
-//   return (
-//     <div className="flex flex-col gap-1">
-//     <span>{title} :</span>
-//     <input
-//       type="text"
-//       placeholder={`${title}...`}
-//       onChange={handleTitle}
-//       className="shadow-md shadow-gray-300
-//     outline-none border-neutral-200 border-1 border py-1 pr-2 rounded-md"
-//     />
-// { inputValue === ""  &&  <p className='text-red-500 '>لطفا
-//     {title} را وارد کنید</p>
-// }  </div>  )
-// }
-
-// export default InputContact
 
 import React, { useState } from 'react'
 
